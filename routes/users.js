6b2860ff50453f82f6cda247a8ea15801709539d
Balazs-D <balazs.danyadi@gmail.com>
2020-02-14 14:36:39 +0100

    JWT added - works

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
@@ -29,7 +31,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
-      if(user) {
+      if (user) {
         return res.status(400).json({ msg: 'User already exists' });
       }
       user = new User({
@@ -41,8 +43,19 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send('User saved');
 
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), {
+          expiresIn: 360000 },
+          (err, token)=> {
+              if(err)throw err;
+              res.json({token});
+          });
     } catch (error) {
       console.error(error.msg);
       res.status(500).send('Server Error');
