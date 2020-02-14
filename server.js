f53f04b6c0408feadd856ff0ca3f9d9ce55e532d
Balazs-D <balazs.danyadi@gmail.com>
2020-02-14 13:13:24 +0100

    validator set up

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const app = express();
 
+//Connect Database 
+connectDB();
+
+// Init middlweware
+app.use(express.json({extended: false}));
 
 // Server setup
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT   }`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Get route: HOME
 app.get('/', ( req,res) => res.json(
     {msg: 'Welcome to the ContactKeeper API'}
     ));
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
